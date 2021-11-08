1a815e9aaed428c0f4dbee019a50fe4df67b9ad5
Lukas Krisna <lukaskrisnaaa@gmail.com>
2021-11-08 19:53:31 +0700

    add image to detail page

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -8,14 +8,16 @@
 </head>
 <body>
     <div class="table-responsive">
-        <table class="table">
+        <table class="table" border="1">
             <thead>
                 <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Address</th>
-                    <th>Telephone</th>
-                    <th>Actions</th>
+                    <th>ID</th>
+                    <th>Judul</th>
+                    <th>Pengarang</th>
+                    <th>Penerbit</th>
+                    <th>Deskripsi</th>
+                    <th>Jumlah Persediaan</th>
+                    <th>Gambar</th>
                 </tr>
             </thead>
             <tbody>
@@ -24,6 +26,9 @@
                     <td>{{ $ebook->judul }}</td>
                     <td>{{ $ebook->pengarang }}</td>
                     <td>{{ $ebook->penerbit }}</td>
+                    <td>{{ $ebook->deskripsi }}</td>
+                    <td>{{ $ebook->persediaan }}</td>
+                    <td><img src="{{ asset('storage/'.$ebook->gambar) }}" alt="gambar-cover-buku" width="200"></td>
                 </tr>
             </tbody>
         </table>
